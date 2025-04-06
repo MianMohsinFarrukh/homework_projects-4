b64febc6d0c943e917d398656627af7d169ab7d2
Mian Mohsin Farrukh <164783660+MianMohsinFarrukh@users.noreply.github.com>
2025-04-06 11:56:22 +0500

    Assignments-00-to-05

---
+++ Assignments-00-to-05/00_intro_python/01_add_two_numbers.py
@@ -0,0 +1,37 @@
+print("This program adds two numbers together.")
+
+def main ():
+    num1 = input("Enter first number: ")
+    num1 = int(num1)
+
+    num2 = input("Enter second number: ")
+    num2 = int(num2)
+
+    total_sum = num1 + num2
+    print(f"The sum of the two numbers is: {total_sum}")
+
+if __name__ == "__main__":
+    main()
+       
+
+
+
+
+#        Problem Statement:
+#          Write a Python program that takes two integer inputs from the user and calculates their sum. The program should perform the following tasks:
+#     Prompt the user to enter the first number.
+
+# Read the input and convert it to an integer.
+
+# Prompt the user to enter the second number.
+
+# Read the input and convert it to an integer.
+
+# Calculate the sum of the two numbers.
+
+# Print the total sum with an appropriate message.
+
+# The provided solution demonstrates a working implementation of this problem, where the main() function guides the user through the process of entering two numbers and displays their sum.
+
+# for GitHub:
+# PROJECTS/homework_projects/00_intro_python/01_add_two_numbers.md
